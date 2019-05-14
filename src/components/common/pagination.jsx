b4f29107ce1f48a50ec8822521b34d027fcca26f
Deyve Machado <deyve.machado@b2wdigital.com>
2019-05-14 13:43:10 -0300

    Pagination Data and Type Checking with PropTypes

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import _ from "lodash";
 
 const Pagination = props => {
@@ -16,8 +17,7 @@ const Pagination = props => {
         {pages.map(page => (
           <li
             key={page}
-            className={page === currentPage ? "page-item active" : "page-item"}
-          >
+            className={page === currentPage ? "page-item active" : "page-item"}>
             <a className="page-link" onClick={() => onPageChange(page)}>
               {page}
             </a>
@@ -26,6 +26,13 @@ const Pagination = props => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itensCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
