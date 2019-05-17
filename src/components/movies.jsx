9cbf207953058da96ee4a8a9fb27ee51152fbb44
Deyve Vieira <deyvedvm@hotmail.com>
2019-05-17 00:43:34 -0300

    React: Sorting - Moving Responsibility

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -49,16 +49,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = path => {
-    const sortColumn = { ...this.state.sortColumn };
-
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-
+  handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
 
@@ -98,6 +89,7 @@ class Movies extends Component {
 
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
