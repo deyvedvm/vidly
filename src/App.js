1bf11a392835a53b890bf18cdf0b8a60b2f8f61d
Deyve Vieira <deyvedvm@hotmail.com>
2019-05-22 00:03:57 -0300

    React: Linking to the MovieForm

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
 
 import "./App.css";
 
@@ -16,6 +17,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
