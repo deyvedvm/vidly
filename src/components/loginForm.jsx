ef1bf308fae97f59ef95dc9acf139f57428276c4
Deyve Vieira <deyvedvm@hotmail.com>
2019-05-24 22:21:13 -0300

    React: Validating a Form Using Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -10,30 +10,30 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
+    const options = {
       abortEarly: false
-    });
+    };
 
-    console.log(result);
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
 
     const errors = {};
 
-    const { account } = this.state;
-
-    if (account.username.trim() === "") {
-      errors.username = "Username is required.";
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
     }
 
-    if (account.password.trim() === "") {
-      errors.password = "Password is required.";
-    }
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    return errors;
   };
 
   validateProperty = ({ name, value }) => {
